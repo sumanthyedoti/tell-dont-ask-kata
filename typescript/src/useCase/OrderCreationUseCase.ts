0975e788d95490d722c0df6d924b4ede004a6275
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-11-24 10:36:05 +0530

    Move exceptions into useCase/exceptions/

--- typescript/src/useCase/OrderCreationUseCase.ts
+++ typescript/src/useCase/OrderCreationUseCase.ts
@@ -1,17 +1,20 @@
-import Order from '../domain/Order';
-import OrderItem from '../domain/OrderItem';
-import { OrderStatus } from '../domain/OrderStatus';
-import Product from '../domain/Product';
-import OrderRepository from '../repository/OrderRepository';
-import { ProductCatalog } from '../repository/ProductCatalog';
-import SellItemsRequest from './SellItemsRequest';
-import UnknownProductException from './UnknownProductException';
+import Order from "../domain/Order";
+import OrderItem from "../domain/OrderItem";
+import { OrderStatus } from "../domain/OrderStatus";
+import Product from "../domain/Product";
+import OrderRepository from "../repository/OrderRepository";
+import { ProductCatalog } from "../repository/ProductCatalog";
+import SellItemsRequest from "./SellItemsRequest";
+import UnknownProductException from "./exceptions/UnknownProductException";
 
 class OrderCreationUseCase {
   private readonly orderRepository: OrderRepository;
   private readonly productCatalog: ProductCatalog;
 
-  public constructor(orderRepository: OrderRepository, productCatalog: ProductCatalog) {
+  public constructor(
+    orderRepository: OrderRepository,
+    productCatalog: ProductCatalog,
+  ) {
     this.orderRepository = orderRepository;
     this.productCatalog = productCatalog;
   }
@@ -20,20 +23,29 @@ class OrderCreationUseCase {
     const order: Order = new Order();
     order.setStatus(OrderStatus.CREATED);
     order.setItems([]);
-    order.setCurrency('EUR');
+    order.setCurrency("EUR");
     order.setTotal(0);
     order.setTax(0);
 
     for (const itemRequest of request.getRequests()) {
-       const product: Product = this.productCatalog.getByName(itemRequest.getProductName());
+      const product: Product = this.productCatalog.getByName(
+        itemRequest.getProductName(),
+      );
 
       if (product === undefined) {
         throw new UnknownProductException();
-      }
-      else {
-        const unitaryTax: number = Math.round(product.getPrice() / 100 * product.getCategory().getTaxPercentage() * 100) / 100;
-        const unitaryTaxedAmount: number = Math.round((product.getPrice() + unitaryTax) * 100) / 100;
-        const taxedAmount: number = Math.round(unitaryTaxedAmount * itemRequest.getQuantity() * 100) / 100;
+      } else {
+        const unitaryTax: number =
+          Math.round(
+            (product.getPrice() / 100) *
+              product.getCategory().getTaxPercentage() *
+              100,
+          ) / 100;
+        const unitaryTaxedAmount: number =
+          Math.round((product.getPrice() + unitaryTax) * 100) / 100;
+        const taxedAmount: number =
+          Math.round(unitaryTaxedAmount * itemRequest.getQuantity() * 100) /
+          100;
         const taxAmount: number = unitaryTax * itemRequest.getQuantity();
 
         const orderItem: OrderItem = new OrderItem();
--- typescript/src/useCase/OrderShipmentUseCase.ts
+++ typescript/src/useCase/OrderShipmentUseCase.ts
@@ -1,16 +1,19 @@
-import Order from '../domain/Order';
-import { OrderStatus } from '../domain/OrderStatus';
-import OrderRepository from '../repository/OrderRepository';
-import { ShipmentService } from '../service/ShipmentService';
-import OrderCannotBeShippedException from './OrderCannotBeShippedException';
-import OrderCannotBeShippedTwiceException from './OrderCannotBeShippedTwiceException';
-import OrderShipmentRequest from './OrderShipmentRequest';
+import Order from "../domain/Order";
+import { OrderStatus } from "../domain/OrderStatus";
+import OrderRepository from "../repository/OrderRepository";
+import { ShipmentService } from "../service/ShipmentService";
+import OrderCannotBeShippedException from "./exceptions/OrderCannotBeShippedException";
+import OrderCannotBeShippedTwiceException from "./exceptions/OrderCannotBeShippedTwiceException";
+import OrderShipmentRequest from "./OrderShipmentRequest";
 
 class OrderShipmentUseCase {
   private readonly orderRepository: OrderRepository;
   private readonly shipmentService: ShipmentService;
 
-  public constructor (orderRepository: OrderRepository, shipmentService: ShipmentService) {
+  public constructor(
+    orderRepository: OrderRepository,
+    shipmentService: ShipmentService,
+  ) {
     this.orderRepository = orderRepository;
     this.shipmentService = shipmentService;
   }
@@ -18,7 +21,10 @@ class OrderShipmentUseCase {
   public run(request: OrderShipmentRequest): void {
     const order: Order = this.orderRepository.getById(request.getOrderId());
 
-    if (order.getStatus() === OrderStatus.CREATED || order.getStatus() === OrderStatus.REJECTED) {
+    if (
+      order.getStatus() === OrderStatus.CREATED ||
+      order.getStatus() === OrderStatus.REJECTED
+    ) {
       throw new OrderCannotBeShippedException();
     }
 
